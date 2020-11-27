4cf03d158af70bb17dde20aee1036d031ff73ac1
lumenghe <lumenghe@gmail.com>
2020-11-27 07:17:34 +0100

    (main): main function

--- kata/app.py
+++ kata/app.py
@@ -4,7 +4,29 @@ import logging
 logger = logging.getLogger(__name__)
 
 def main():
-    pass
+    """ main function """
+    first_player = Player("Nathanael")
+    second_player = Player("Gabriel")
+    game = Kata(first_player, second_player)
+    print(game.get_result())
+    first_player.get_one_score()
+    print(game.get_result())
+    second_player.get_one_score()
+    print(game.get_result())
+    second_player.get_one_score()
+    print(game.get_result())
+    first_player.get_one_score()
+    print(game.get_result())
+    first_player.get_one_score()
+    print(
+        "score name first({}): second({})".format(
+            first_player.score_name(), second_player.score_name()
+        )
+    )
+    print(game.get_result())
+    first_player.get_one_score()
+    print(game.get_result())
+
 
 if __name__ == "__main__":
     main()
